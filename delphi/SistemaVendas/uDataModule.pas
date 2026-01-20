1b0b8e8e1f156bd804d3386e8892a50b153b1ff8
paulomussi <pauloricardomuss@gmail.com>
2026-01-20 13:44:07 -0300

    tela cidades

---
+++ delphi/SistemaVendas/uDataModule.pas
@@ -0,0 +1,35 @@
+unit uDataModule;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, Data.DB, Data.Win.ADODB;
+
+type
+  TdmConexao = class(TDataModule)
+    conexao: TADOConnection;
+    qryCidades: TADOQuery;
+    dsCidades: TDataSource;
+    procedure DataModuleCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  dmConexao: TdmConexao;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+procedure TdmConexao.DataModuleCreate(Sender: TObject);
+begin
+  conexao.Connected := True;
+  qryCidades.Open;
+end;
+
+end.
